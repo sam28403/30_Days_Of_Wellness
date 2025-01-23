aa24afdc18551af256424900b40ad266bb0a09da
sam28403 <hefeixuan28403@gmail.com>
2025-01-23 11:19:12 +0800

    Update Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "cc.samlab.wellness"
         minSdk = 28
         targetSdkPreview = "Baklava"
-        versionCode = 9
-        versionName = "1.0.8"
+        versionCode = 10
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
